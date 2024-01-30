b52f35b7a5f40501b6737a9a5935152855e666da
Clarity-89 <homes89@ukr.net>
2024-01-30 16:21:16 +0200

    Handle icon size

--- src/IconBase.tsx
+++ src/IconBase.tsx
@@ -10,27 +10,47 @@ export interface IconProps
   color?: string;
 }
 
+function getSvgSize(size: IconSize) {
+  const sizeMap = {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
+    xxl: 36,
+    xxxl: 48,
+  };
+
+  return sizeMap[size] || 16;
+}
+
 export const IconBase = forwardRef<SVGSVGElement, IconProps>(
-  ({ color, title, size, className, ...props }, ref) => (
-    <svg
-      ref={ref}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill={color || "currentColor"}
-      className={cx(
-        css({
-          display: "inline-block",
-          fill: "currentColor",
-          flexShrink: 0,
-          label: "Icon",
-          lineHeight: 0,
-          verticalAlign: "middle",
-        }),
-        className,
-      )}
-      {...props}
-    >
-      {props.children}
-    </svg>
-  ),
+  ({ color, title, size = "md", className, ...props }, ref) => {
+    const svgSize = getSvgSize(size);
+
+    return (
+      <svg
+        ref={ref}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill={color || "currentColor"}
+        height={svgSize}
+        width={svgSize}
+        className={cx(
+          css({
+            display: "inline-block",
+            fill: "currentColor",
+            flexShrink: 0,
+            label: "Icon",
+            lineHeight: 0,
+            verticalAlign: "middle",
+          }),
+          className,
+        )}
+        {...props}
+      >
+        {props.children}
+      </svg>
+    );
+  },
 );
